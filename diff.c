1daeeb8480e96104f17758cba4b103edd720a006
Sacha Béraud <sacha.beraud@gmail.com>
2012-03-21 11:43:49 +1100

    c code improved

--- diff.c
+++ diff.c
@@ -21,18 +21,29 @@ int main(void)
     
     int c[N][N] =   {0};
     
-    int i;
-    int j;
-    int k;
+    int i = 0;
+    int j = 0;
+    int k = 0;
     int x,y;
     
-    for( i=0;i < N; i++) {
-        
-        for(j=0; j < N; j++) {
-            for (k=0; k<N; k++) {
+    while(i < N) {
+        j=0;
+        while(j < N) {
+            k=0;
+            while (k<N) {
                 printf(" i = %d   j = %d   k = %d\n", i, j, k);
-                c[i][j] += a[i][k] - b[k][j];
                 
+                c[i][j] += a[i][k] - b[k][j];
+                //taking care of abs
+                if (c[i][j] < 0) {
+                    c[i][j] = -c[i][j];
+                }
+                
+                //taking care of OVF
+                if(c[i][j] >= 127) {
+                    c[i][j] = 127;
+                }
+                //debug
                 for (x=0; x< N; x++) {
                     for(y=0; y<N; y++) {
                         printf("  %d", c[x][y]);
@@ -40,13 +51,13 @@ int main(void)
                     printf("\n");
                 }
                 printf("\n");
+                //enddebug
                 
+                k++;
             }
-            
-            
+            j++;
         }
-        
-        
+        i++;
     }
     
     printf("I'm done calulating: here is my result of C\n");
